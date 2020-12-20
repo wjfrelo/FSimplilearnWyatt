a73c414dd36373153b890e6a4b69167cfb26a968
Wyatt Frelot <wfrelot@cloudera.com>
2020-12-20 08:47:38 -0500

    Added additional information for today's lesson

---
+++ Lesson-6-Automated-Testing/info/Lesson-6.4/script.sh
@@ -0,0 +1,5 @@
+wget https://mirrors.ocf.berkeley.edu/apache//jmeter/binaries/apache-jmeter-5.4.tgz -c $HOME
+
+tar xvzf $HOME/apache-jmeter-5.4.tgz -C $HOME
+
+$HOME/apache-jmeter-5.4/bin/./jmeter.sh
