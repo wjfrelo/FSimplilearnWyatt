cb299d99d0d512c81aac097f672206249a0edd4e
Wyatt Frelot <wfrelot@cloudera.com>
2021-02-07 11:18:25 -0500

    updates

--- Lesson-6-Automated-Testing/class-demos/example1.code/Calculator.java
+++ Lesson-6-Automated-Testing/class-demos/example1.code/Calculator.java
@@ -3,12 +3,12 @@ package jenkinsDemo;
 public class Calculator {
 
     public int multiply(int a, int b) {
-        if (a = 0  || b = 0 ) {
+        if (a == 0 || b == 0)
             return 0;
-        }
         else {
             return a * b;
         }
+
     }
 
     public int add (int a, int b) {
